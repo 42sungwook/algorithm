690b35c7e9e5415df5c785210846528358e96816
sungwook <106149304+42sungwook@users.noreply.github.com>
2024-09-25 18:31:38 +0900

    [Bronze II] Title: 숫자의 개수, Time: 80 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
@@ -2,13 +2,11 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 
 const [A, B, C] = input.map(Number);
 
-const num = String(A * B * C);
-const arr = [...num];
-const len = arr.length;
-const ans = new Array(10).fill(0);
-for (let i = 0; i < len; i++){
-    ans[arr[i]]++;
+const result = (A * B * C).toString();
+const counts = new Array(10).fill(0);
+
+for (let digit of result) {
+    counts[digit]++;
 }
-for (let i = 0; i < 10; i++) {
-    console.log(ans[i]);
-}
+
+console.log(counts.join('\n'));
