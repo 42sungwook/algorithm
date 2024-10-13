8acffb2459556087c308b5cd8273384806f4b8f7
sungwook <106149304+42sungwook@users.noreply.github.com>
2024-10-13 13:13:50 +0900

    [Bronze II] Title: Hashing, Time: 88 ms, Memory: 9328 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -0,0 +1,17 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+function calHash(str) {
+  const M = 1234567891
+  const r = 31
+  let hash = 0
+  let pow = 1
+
+  for (let i = 0; i < str.length; i++) {
+    const charCode = str.charCodeAt(i) - 'a'.charCodeAt(0) + 1
+		hash = (hash + charCode * pow) % M
+		pow = (pow * r)
+  }
+	return hash
+}
+
+console.log(calHash(input[1]))
