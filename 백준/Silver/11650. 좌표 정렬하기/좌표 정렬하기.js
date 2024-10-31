51436140b78b581996c61e6830a523fe0cc320fc
sungwook <106149304+42sungwook@users.noreply.github.com>
2024-10-31 20:38:18 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 3840 ms, Memory: 62936 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,20 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+list = []
+for (let i = 1; i < input.length; i++) {
+  const coor = input[i].split(' ').map(Number)
+  list.push(coor)
+}
+
+function sortCoor(coorList) {
+  coorList.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1]
+    }
+    return a[0] - b[0]
+  })
+  return coorList
+}
+
+const sortedList = sortCoor(list)
+sortedList.forEach((coor) => console.log(coor.join(' ')))
