a8625c416ae4528b5f6f3ec5d593266d8af7a281
sungwook <106149304+42sungwook@users.noreply.github.com>
2025-09-03 23:43:09 +0900

    [Silver IV] Title: 제로, Time: 172 ms, Memory: 16068 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.js
+++ 백준/Silver/10773. 제로/제로.js
@@ -1,16 +1,24 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number)
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number)
 
+const K = input[0]
 const stack = []
-let sum = 0
 
-for (let i = 1; i < input.length; i++) {
+for (let i = 1; i <= K; i++) {
   if (input[i] === 0) {
     stack.pop()
   } else {
     stack.push(input[i])
   }
 }
-if (stack.length) {
-  sum = stack.reduce((a, b) => a + b)
+
+if (stack.length === 0) {
+  console.log(0)
+  return
 }
-console.log(sum)
+
+console.log(stack.reduce((prev, curr) => prev + curr))
